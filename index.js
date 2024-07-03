95c332cf5f1b641f792b4a879c8d5de5542b88c1
shawukie4them <shawukiekawuki@gmail.com>
2024-07-03 12:00:13 +0300

    array

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Write your solution here!
+//Exercise 1
+let cats = ["Milo", "Otis", "Garfield"];
+
+//Exercise 2 
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+
+//Exercise 3 
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+
+//Exercise 4 
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+//Exercise 5 
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+//Exercise 6 
+function appendCat(name){
+    return [...cats, name];
+}
+
+//Exercise 7 
+function prependCat(name){
+    return [name, ...cats];
+}
+
+//Exercise 8 
+function removeLastCat(){
+    return cats.slice(0, cats.length - 1);
+}
+
+//Exercise 9
+function removeFirstCat(){
+    return cats.slice(1);
+}
